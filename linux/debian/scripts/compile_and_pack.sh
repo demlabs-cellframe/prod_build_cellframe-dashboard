3b204ae1aae7e3b7a614ad267ff8d93b474b7b6b
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-13 13:35:14 +0300

    remove deb pkges with dbgsym

--- linux/debian/scripts/compile_and_pack.sh
+++ linux/debian/scripts/compile_and_pack.sh
@@ -28,7 +28,11 @@ error=0
 #2DO: add trap command to clean the sources on exit.
 trap cleanup SIGINT
 codename=$(lsb_release -a | grep Codename | cut -f2)
-dpkg-buildpackage -J -us --changes-option=--build=any -uc && mkdir -p build && \
+dpkg-buildpackage -J -us --changes-option=--build=any -uc
+if [[ $(ls .. | grep 'dbgsym') != "" ]]; then
+	rm -f *dbgsym*
+fi
+mkdir -p build && \
 for filepkg in $(ls .. | grep .deb | grep -v $codename); do
 	mv ../$filepkg build/$filepkg
 done || error=$?
