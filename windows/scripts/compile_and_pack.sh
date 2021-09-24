dbc15860a0c7e54b9a608259c9e22c7af62eef20
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-24 23:12:19 +0300

    rewrite compile for windows build

--- windows/scripts/compile_and_pack.sh
+++ windows/scripts/compile_and_pack.sh
@@ -3,6 +3,10 @@ WINDOWS_TOOLCHAIN=$WINDOWS_TOOLCHAIN_PATH/usr/bin
 WINDOWS_CROSS_QT=$WINDOWS_TOOLCHAIN_PATH/usr/x86_64-w64-mingw32.static/qt5/bin
 export PATH=$WINDOWS_TOOLCHAIN:$PATH
 
+. prod_build/general/conf/version_info
+. prod_build/general/pre-build.sh
+. prod_build/general/conf/publish
+
 error_explainer() {
 
 	case "$1" in
@@ -25,48 +29,44 @@ fi
 
 }
 
-defines() {
+# defines() {
 
-[ -e ./build/Nsis.defines.nsh ] && rm ./build/Nsis.defines.nsh
-echo "!define APP_NAME \"$1\"" >> ./build/Nsis.defines.nsh
-echo "!define APP_VERSION \"$(echo $2 | sed 's/-/\./g').0\"" >> ./build/Nsis.defines.nsh
-echo "!define DAP_VER \"$2\"" >> ./build/Nsis.defines.nsh
+# [ -e ./build/Nsis.defines.nsh ] && rm ./build/Nsis.defines.nsh
+# echo "!define APP_NAME \"$1\"" >> ./build/Nsis.defines.nsh
+# echo "!define APP_VERSION \"$(echo $2 | sed 's/-/\./g').0\"" >> ./build/Nsis.defines.nsh
+# echo "!define DAP_VER \"$2\"" >> ./build/Nsis.defines.nsh
 
-}
+# }
 
-. prod_build/general/conf/version_info
-. prod_build/general/pre-build.sh
+# pack() {
 
-pack() {
-
-	mkdir -p ./build/
-	rm -r ./build/*
-	cp ./CellFrameDashboardGUI/resources/icons/icon_windows.ico ./build/
-	#cp -r ./os/windows/drivers ./build/
-	cp ./os/windows/build.nsi ./build/
-	cp ./prod_build/windows/nsis/ibeay32.dll ./build/
-	cp ./prod_build/windows/nsis/ssleay32.dll ./build/	
-	VERSION=$(extract_version_number)
-	defines $1 $VERSION
-	cp ./CellFrameDashboardService/release/CellFrameDashboardService.exe ./build/
-	cp ./CellFRameDashboardGUI/release/CellFrameDashboard.exe ./build/
-	makensis ./build/build.nsi
-	mv ./build/"$BRAND ${VERSION}.exe" ./build/"${BRAND}-${VERSION}.exe"
-}
-
-. prod_build/general/conf/publish
+# 	mkdir -p ./build/
+# 	rm -r ./build/*
+# 	cp ./brand/$1/DapChainVpnGui/resources/pics/icon_app.ico ./build/
+# 	cp -r ./os/windows/drivers ./build/
+# 	cp ./os/windows/build.nsi ./build/
+# 	cp ./os/windows/ssl/libeay32.dll ./build/
+# 	cp ./os/windows/ssl/ssleay32.dll ./build/	
+# 	VERSION=$(extract_version_number)
+# 	defines $1 $VERSION
+# 	cp ./DapChainVpnService/release/KelVPNService.exe ./build/
+# 	cp ./DapChainVpnGui/release/KelVPN.exe ./build/
+# 	makensis ./build/build.nsi
+# 	mv ./build/"$BRAND ${VERSION}.exe" ./build/"${BRAND}-${VERSION}.exe"
+# }
 
 error=0
 #codename=$(lsb_release -a | grep Codename | cut -f2)
 #2DO: add trap command to clean the sources on exit.
+
+cd cellframe-node && git submodule update --init && cd -
+sed -i 's/#nsis_build/nsis_build/g' CellFRameDashboardGUI/CellFRameDashboardGUI.pro
+sed -i 's/#nsis_build/nsis_build/g' CellFrameDashboardService/CellFrameDashboardService.pro
+
 trap cleanup SIGINT
-	cd cellframe-node && git checkout master && git pull && git submodule init && git submodule update
-	mkdir build && cd build
-	x86_64-w64-mingw32.static-cmake .. && make
-	$WINDOWS_CROSS_QT/qmake && \
-	make -j$(nproc) && \
-	[ -v BRAND ] && echo "Brand = $BRAND" || { echo "No brand defined"; BRAND="CellFrameDashboard"; } && \
-#	pack $BRAND
+	#[ -v BRAND ] && echo "Brand = $BRAND" || { echo "No brand defined"; BRAND="CellFrameDashboard"; } && \
+	$WINDOWS_CROSS_QT/qmake && make -j$(nproc)
+	# pack $BRAND
 #	for filepkg in $(ls .. | grep .deb | grep -v $codename | grep -v "dbgsym"); do
 #		filename=$(echo $filepkg | sed 's/.deb$//')
 #		[ ! -v QT_LINUX_PATH ] && mv ../$filepkg build/$filename\_$codename.deb || mv ../$filepkg build/$filepkg
