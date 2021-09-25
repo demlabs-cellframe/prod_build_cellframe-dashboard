144f4db733b710a352087ad43142e70100807c32
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-25 18:59:12 +0300

    add ref_name into name of pkg

--- general/post-build.sh
+++ general/post-build.sh
@@ -6,6 +6,12 @@ errcode=0
 
 . prod_build/general/pre-build.sh
 export_variables $(find "./prod_build/$platform/conf" -maxdepth 1 -type f)
+export_variables "prod_build/general/conf/*"
+
+if [[ $CI_COMMIT_REF_NAME != "" ]] && [[ $CI_COMMIT_REF_NAME != "master" ]]; then
+	export MOD="${MOD} ${CI_COMMIT_REF_NAME}"
+fi
+MOD=$( echo $MOD | sed 's/ /-/g')
 
 [[ -e prod_build/$platform/scripts/post-build.sh ]] && prod_build/$platform/scripts/post-build.sh $platform || { errcode=$? && errstring="$errstring ${platform}_postbuild errcode $errcode"; } #For post-build actions not in chroot (global publish)
 [[ $errstring != "" ]] && echo "$brand done with errors:" && echo "$errstring" >> ~/prod_log && errstring="" && errcode=5 ## General failure error
