f7da1ec33e9dbe3183a36b59c46b2a6fe2dd4c71
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-25 19:15:51 +0300

    some changes for better build in docker && fixed errors

--- general/post-build.sh
+++ general/post-build.sh
@@ -8,10 +8,12 @@ errcode=0
 export_variables $(find "./prod_build/$platform/conf" -maxdepth 1 -type f)
 export_variables "prod_build/general/conf/*"
 
+echo "Mod handler"
 if [[ $CI_COMMIT_REF_NAME != "" ]] && [[ $CI_COMMIT_REF_NAME != "master" ]]; then
 	export MOD="${MOD} ${CI_COMMIT_REF_NAME}"
 fi
 MOD=$( echo $MOD | sed 's/ /-/g')
+
 
 [[ -e prod_build/$platform/scripts/post-build.sh ]] && prod_build/$platform/scripts/post-build.sh $platform || { errcode=$? && errstring="$errstring ${platform}_postbuild errcode $errcode"; } #For post-build actions not in chroot (global publish)
 [[ $errstring != "" ]] && echo "$brand done with errors:" && echo "$errstring" >> ~/prod_log && errstring="" && errcode=5 ## General failure error
