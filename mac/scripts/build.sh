67743be2aa46617f12ca3ffa6ae7757d19012cd4
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-28 13:31:48 +0300

    build mac

---
+++ mac/scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NODE_PATH=./cellframe-node
+
+# compile cellframe-dashboard
+./prod_build/mac/scripts/compile.sh $1 || { errcode=$?; echo "[ERR] Mac build errcode $errcode";exit $errcode; }
+
+# compile cellframe-node
+./cellframe-node/prod_build/mac/scripts/compile.sh $NODE_PATH $BUILD_PATH
+#./prod_build/mac/scripts/addqt.sh $1 || { errcode=$?; echo "[ERR] Mac addqtlibs errcode $errcode";exit $errcode; }
+#./prod_build/mac/scripts/reloc.sh $1 || { errcode=$?; echo "[ERR] Mac reloc errcode $errcode"; exit $errcode; }
+#./prod_build/mac/scripts/sign.sh $1 || exit 6
+./prod_build/mac/scripts/pack.sh $1 || { errcode=$?; echo "[ERR] Mac pack errcode $errcode"; exit $errcode; }
+#exit 0
