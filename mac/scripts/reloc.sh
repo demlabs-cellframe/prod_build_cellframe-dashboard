dd82e2e9cb9bfb30da0d210cda46338f830bb144
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-10-06 18:58:18 +0300

    update error handler in relloc

--- mac/scripts/reloc.sh
+++ mac/scripts/reloc.sh
@@ -1,43 +1,59 @@
     #!/bin/bash
     #set up config
 
+    error=0
+
   
     mkdir -p $wd/$BUILD_PATH
     cp -r $APP_PATH $wd/$BUILD_PATH
 
     if [ -e $wd/CellFrameDashboardService/"$APP_SERVICE_NAME" ]; then
         mv -f $wd/CellFrameDashboardService/"$APP_SERVICE_NAME"  $wd/$BUILD_PATH/$APP_NAME.app/Contents/MacOS
+     else
+        echo "[ERR] Not found $wd/CellFrameDashboardService/"$APP_SERVICE_NAME"" && error=1;
     fi
 
  	if [ -e $wd/cellframe-node/build/"$NODE_NAME" ]; then
         mv -f $wd/cellframe-node/build/"$NODE_NAME"  $wd/$BUILD_PATH/$APP_NAME.app/Contents/MacOS
+     else
+        echo "[ERR] Not found $wd/cellframe-node/build/"$NODE_NAME"" && error=1;
     fi
 
 	if [ -e $wd/cellframe-node/build/"$NODE_CLI_NAME" ]; then
         mv -f $wd/cellframe-node/build/"$NODE_CLI_NAME"  $wd/$BUILD_PATH/$APP_NAME.app/Contents/MacOS
+    else
+        echo "[ERR] Not found $wd/cellframe-node/build/"$NODE_CLI_NAME"" && error=1;
     fi
 
 	if [ -e $wd/cellframe-node/build/"$NODE_TOOL_NAME" ]; then
         mv -f $wd/cellframe-node/build/"$NODE_TOOL_NAME"  $wd/$BUILD_PATH/$APP_NAME.app/Contents/MacOS
+     else
+        echo "[ERR] Not found $wd/cellframe-node/build/"$NODE_TOOL_NAME"" && error=1;
     fi
 
-    if [ -e $wd/prod_build/mac/essentials/com*Cellframe-DashboardService.plist ]; then
-         cp -f $wd/prod_build/mac/essentials/com*Cellframe-DashboardService.plist $wd/$BUILD_PATH/$APP_NAME.app/Contents/Resources/
+    if [ -e $wd/prod_build/mac/essentials/com*CellFrameDashboardService.plist ]; then
+         cp -f $wd/prod_build/mac/essentials/com*CellFrameDashboardService.plist $wd/$BUILD_PATH/$APP_NAME.app/Contents/Resources/
+     else
+        echo "[ERR] Not found $wd/prod_build/mac/essentials/com*CellFrameDashboardService.plist" && error=1;
     fi
 
  	if [ -e $wd/prod_build/mac/essentials/com*cellframe-node.plist ]; then
          cp -f $wd/prod_build/mac/essentials/com*cellframe-node.plist $wd/$BUILD_PATH/$APP_NAME.app/Contents/Resources/
+     else
+        echo "[ERR] Not found $wd/prod_build/mac/essentials/com*cellframe-node.plist" && error=1;
     fi
 	
     if [ -e $wd/prod_build/mac/essentials/cleanup ]; then
          cp -rf $wd/prod_build/mac/essentials/cleanup $wd/$BUILD_PATH/$APP_NAME.app/Contents/Resources/
+     else
+        echo "[ERR] Not found $wd/prod_build/mac/essentials/cleanup" && error=1;
     fi
 
      
     if [ -e $wd/prod_build/mac/essentials/$APP_NAME-pkginstall ]; then
         cp -rf $wd/prod_build/mac/essentials/$APP_NAME-pkginstall/* $wd/$BUILD_PATH
     else
-        echo "[ERR] Not found pkginstall files" && exit 1;
+        echo "[ERR] Not found $wd/prod_build/mac/essentials/$APP_NAME-pkginstall" && error 1;
     fi
     
 
@@ -46,4 +62,4 @@
 	cp -f $wd/cellframe-node/scripts/* $wd/$BUILD_PATH/$APP_NAME.app/Contents/MacOS
 	cp -f $wd/prod_build/mac/essentials/create_configs.sh $wd/$BUILD_PATH/$APP_NAME.app/Contents/MacOS
 
-    exit 0
+    exit $error
