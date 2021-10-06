f07079c09e732dfda99643f32439a099390f5646
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-10-06 21:00:44 +0300

    upd

--- mac/scripts/post-build.sh
+++ mac/scripts/post-build.sh
@@ -6,4 +6,4 @@ platform=$1
 SCRIPTDIR="prod_build/$platform/scripts"
 
 $SCRIPTDIR/deploy.sh || { echo "[ERR] $platform deploy error" && exit 20; }
-$SCRIPTDIR/cleanup.sh || { echo "[ERR] $platform cleanup error" && exit 21; }
+#$SCRIPTDIR/cleanup.sh || { echo "[ERR] $platform cleanup error" && exit 21; }
