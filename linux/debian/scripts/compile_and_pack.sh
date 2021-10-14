66f5aeaa33fb4e5fd4bf2e9b0f3e0ab274779930
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-10-14 16:00:09 +0300

    cleaning

--- linux/debian/scripts/compile_and_pack.sh
+++ linux/debian/scripts/compile_and_pack.sh
@@ -24,21 +24,21 @@ fi
 
 }
 
+changelog_fix () {
+	versionMaj=$(cat config.pri | grep 'VER_MAJ =' | cut -d'=' -f 2 | sed s/' '//g)
+	versionMin=$(cat config.pri | grep 'VER_MIN =' | cut -d'=' -f 2 | sed s/' '//g)
+	versionPatch=$(cat config.pri | grep 'VER_PAT =' | cut -d'=' -f 2 | sed s/' '//g)
+	sed  -i "0,/$versionMaj.$versionMin-[0-9]\+/{s//$versionMaj.$versionMin-$versionPatch/}" debian/changelog
+	sed  -i "s/$versionMaj.$versionMin-[0-9]\+/$versionMaj.$versionMin-$versionPatch/" debian/control
+	sed -i 's/(HEAD detached at .*)/develop/' debian/changelog
+}
+
 error=0
 #2DO: add trap command to clean the sources on exit.
 trap cleanup SIGINT
 codename=$(lsb_release -a | grep Codename | cut -f2)
 
-# fixed changelog
-
-versionMaj=$(cat config.pri | grep 'VER_MAJ =' | cut -d'=' -f 2 | sed s/' '//g)
-versionMin=$(cat config.pri | grep 'VER_MIN =' | cut -d'=' -f 2 | sed s/' '//g)
-versionPatch=$(cat config.pri | grep 'VER_PAT =' | cut -d'=' -f 2 | sed s/' '//g)
-sed  -i "0,/$versionMaj.$versionMin-[0-9]\+/{s//$versionMaj.$versionMin-$versionPatch/}" debian/changelog
-sed  -i "s/$versionMaj.$versionMin-[0-9]\+/$versionMaj.$versionMin-$versionPatch/" debian/control
-sed -i 's/(HEAD detached at .*)/develop/' debian/changelog
-cat debian/changelog
-cat debian/control
+changelog_fix
 
 dpkg-buildpackage -J -us --changes-option=--build=any -uc || error=$?
 if [[ $(ls .. | grep 'dbgsym') != "" ]]; then
