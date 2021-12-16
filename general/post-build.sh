e749ad4d62f74ab2b7debf247cf797138b6d5dc0
aleksandr.vodianov@demlabs.net <aleksandr.vodianov@demlabs.net>
2021-12-16 19:11:11 +0300

    added build for am64e architecture

--- general/post-build.sh
+++ general/post-build.sh
@@ -11,11 +11,13 @@ for conffile in $(find "./prod_build/$platform/conf" | grep conf/ | grep -v .bak
 done
 export_variables "prod_build/general/conf/*"
 
+echo $platform
+echo $PACKAGE_PATH
 echo "Mod handler"
 if [[ $CI_COMMIT_REF_NAME != "" ]] && [[ $CI_COMMIT_REF_NAME != "master" ]]; then
 	export MOD="-${CI_COMMIT_REF_NAME}"
 fi
-
+echo $MOD
 
 [[ -e prod_build/$platform/scripts/post-build.sh ]] && prod_build/$platform/scripts/post-build.sh $platform || { errcode=$? && errstring="$errstring ${platform}_postbuild errcode $errcode"; } #For post-build actions not in chroot (global publish)
 [[ $errstring != "" ]] && echo "$brand done with errors:" && echo "$errstring" >> ~/prod_log && errstring="" && errcode=5 ## General failure error
--- mac/scripts/build.sh
+++ mac/scripts/build.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+arch=$1
 NODE_PATH=./cellframe-node
 
-# compile cellframe-dashboard
-./prod_build/mac/scripts/compile.sh $1 || { errcode=$?; echo "[ERR] Mac build errcode $errcode";exit $errcode; }
+export QT_MAC_PATH=${OSXCROSS_PATH}/$arch/${QT_PATH}
+export CROSS_COMPILE=${arch}${OSX_COMPILE}
 
+sed -i "s/arm64/arm64e/g" cellframe-node/cellframe-sdk/3rdparty/monero_crypto/CMakeLists.txt
+
+# compile cellframe-dashboard
+./prod_build/mac/scripts/compile.sh $arch || { errcode=$?; echo "[ERR] Mac build errcode $errcode";exit $errcode; }
 # compile cellframe-node
 echo "[INF] Compile cellframe-node"
 ./cellframe-node/prod_build/mac/scripts/compile.sh $NODE_PATH || { errcode=$?; echo "[ERR] Mac cellframe-node compile $errcode"; exit $errcode; }
 #./prod_build/mac/scripts/addqt.sh $1 || { errcode=$?; echo "[ERR] Mac addqtlibs errcode $errcode";exit $errcode; }
-./prod_build/mac/scripts/reloc.sh $1 || { errcode=$?; echo "[ERR] Mac reloc errcode $errcode"; exit $errcode; }
+./prod_build/mac/scripts/reloc.sh || { errcode=$?; echo "[ERR] Mac reloc errcode $errcode"; exit $errcode; }
 #./prod_build/mac/scripts/sign.sh $1 || exit 6
-./prod_build/mac/scripts/pack.sh $1 || { errcode=$?; echo "[ERR] Mac pack errcode $errcode"; exit $errcode; }
-#./prod_build/mac/scripts/clean.sh || { errcode=$?; echo "[ERR] Mac clean errcode $errcode"; exit $errcode; }
+./prod_build/mac/scripts/pack.sh $arch || { errcode=$?; echo "[ERR] Mac pack errcode $errcode"; exit $errcode; }
 make distclean
+rm .qmake.stash
+
 exit 0
