391dd8de42f57e31c50270e0bbfe78a9cf451434
aleksandr.vodianov@demlabs.net <aleksandr.vodianov@demlabs.net>
2022-01-19 22:16:13 +0300

    added node's build target

---
+++ linux/debian/scripts/compile_node.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wd=$1
+
+cd $wd
+mkdir -p build && cd build
+
+sed -i 's/target_link_libraries(${NODE_TARGET}      ${NODE_LIBRARIES} pthread )/target_link_libraries(${NODE_TARGET}      ${NODE_LIBRARIES} pthread z util expat )/' ../CMakeLists.txt && \
+	${CMAKE_PATH}cmake ../ && make -j$(nproc) || error=$?
+
+exit $error
