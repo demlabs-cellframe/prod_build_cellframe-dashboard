5710b20252fe291d4353e52720bef69b2a3e706a
aleksandr.vodianov@demlabs.net <aleksandr.vodianov@demlabs.net>
2022-02-09 18:36:00 +0300

    fixed deploy

--- android/scripts/deploy.sh
+++ android/scripts/deploy.sh
@@ -12,21 +12,20 @@ echo $PKGFILES
 
 errcode=0
 for pkgfile in $PKGFILES; do
-pkgname=$(echo $pkgfile | sed "s/.apk$//")
+	pkgname=$(echo $pkgfile | sed "s/.apk$//")
 	echo $VERSION_STRING
 	echo "Pkgfile is $pkgfile"
 	pkgname_weblink="$(echo $pkgname | sed 's/-\?[0-9]\+\.[0-9]\+-[0-9]\+//' )-latest" #leaving only necessary entries
-	mv -v $pkgfile $PACKAGE_PATH/$pkgname$MODNAME.apk || echo "[ERR] Something went wrong in publishing the package. Now aborting."
+	mv -v $pkgfile $PACKAGE_PATH/$pkgname.apk || echo "[ERR] Something went wrong in publishing the package. Now aborting."
 	# if [[ -v CI_COMMIT_REF_NAME ]] && { [[ $CI_MERGE_REQUEST_TARGET_BRANCH_NAME == "master" ]] || [[ $(echo $CI_COMMIT_REF_NAME | grep "master\|^release\|^pubtest\|^hotfix") ]]; }; then #TMP restriction for master only.
 	CODENAME=$(echo $pkgname | rev | cut -d '_' -f1 | rev)
 	cp -r ../../prod_build/general/essentials/weblink-latest ../../prod_build/general/essentials/$pkgname_weblink
-	sed -i "/document/s/\/.*deb/\/$pkgname$MODNAME.apk/" ../../prod_build/general/essentials/$pkgname_weblink/index.php
+	sed -i "/document/s/\/.*deb/\/$pkgname.apk/" ../../prod_build/general/essentials/$pkgname_weblink/index.html
 	echo "attempting to publish new Cellframe-Dashboard"
-	[[ $SUBDIR != "" ]] && ssh -i $CELLFRAMRE_FILESERVER_KEY "$CELLFRAMRE_FILESERVER_CREDS" "mkdir -p $CELLFRAMRE_FILESERVER_PATH/android/$SUBDIR"
-	scp -i $CELLFRAMRE_FILESERVER_KEY $PACKAGE_PATH/$pkgname$MODNAME.apk "$CELLFRAMRE_FILESERVER_CREDS:$CELLFRAMRE_FILESERVER_PATH/android/$SUBDIR"
-	scp -r -i $CELLFRAMRE_FILESERVER_KEY ../../prod_build/general/essentials/$pkgname_weblink "$CELLFRAMRE_FILESERVER_CREDS:$CELLFRAMRE_FILESERVER_PATH/android/$SUBDIR"
+	[[ $SUBDIR != "" ]] && ssh -i $CELLFRAME_FILESERVER_KEY "$CELLFRAME_FILESERVER_CREDS" "mkdir -p $CELLFRAME_FILESERVER_PATH/android/$SUBDIR"
+	scp -i $CELLFRAME_FILESERVER_KEY $PACKAGE_PATH/$pkgname$MODNAME.apk "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/android/$SUBDIR"
+	scp -r -i $CELLFRAME_FILESERVER_KEY ../../prod_build/general/essentials/$pkgname_weblink "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/android/$SUBDIR"
 	rm -r ../../prod_build/general/essentials/$pkgname_weblink
-	# fi
 done
 cd ../..
 exit $errcode
