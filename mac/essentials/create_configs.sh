36a819f2b45736a765e6ea728523e7c16f691735
Denis Sumin <denis.smolov@demlabs.net>
2022-04-08 16:09:16 +0000

    Update create_configs.sh

--- mac/essentials/create_configs.sh
+++ mac/essentials/create_configs.sh
@@ -15,13 +15,6 @@ export DAP_SERVER_PORT=8089
 export DAP_NOTIFY_SRV_ADDRESS=127.0.0.1
 export DAP_NOTIFY_SRV_PORT=8080
 
-# DapCash testnet
-#export DAP_CORE_T_ENABLED=true
-#export DAP_CORE_T_ROLE=full
-
-# Kelvin testnet
-#export DAP_KELVIN_TESTNET_ENABLED=true
-#export DAP_KELVIN_TESTNET_ROLE=full
 
 # Subzero testnet
 export DAP_SUBZERO_ENABLED=true
